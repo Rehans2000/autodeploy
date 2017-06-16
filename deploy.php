48aa7f7e52d86e019e19df6773d92a230b01efe4
Rehan <networks@accunity.com>
2017-06-16 13:35:33 +0500

    added test

--- deploy.php
+++ deploy.php
@@ -1,15 +1,4 @@
 <?php
-if (!isset($_SERVER['HTTP_X_EVENT_KEY'])) {
-    header('HTTP/1.0 403 Forbidden');
-    echo '<h1>403 Forbidden</h1>';
-    echo 'You do not have access to view the contents of this web page';
-    die();
-}
-
-$data = json_decode(file_get_contents('php://input'), true);
 
     getcwd();
-    shell_exec('git stash');
     shell_exec('git pull');
-    shell_exec('git stash pop stash@{0}');
-}
