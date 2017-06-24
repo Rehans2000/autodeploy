9a34a052768bcdc0f55e45dcb4f30181914e2a4d
rehan <rehan@octraves.com>
2017-06-24 10:17:07 +0500

    rejsn

---
+++ myhook.php
@@ -0,0 +1,12 @@
+// Your remote name
+$remote = "origin";
+
+// Aliases for branches and directories
+$aliases = array(
+  "master"  => array( "path/to/production" ),
+  "staging" => "/var/www/html",
+  "clients" => array( "client1","client2","client3","client4" )
+);
+
+// Do you want a log file with web hook posts?
+$log = FALSE;x
